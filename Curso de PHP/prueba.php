403e3e46bc40ee8cda08e557713b8af9fe59e7da
iDoiatxu <akirintxa@gmail.com>
2022-12-15 15:52:23 -0400

    Adding Curso de PHP

---
+++ Curso de PHP/prueba.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Hola Mundo desde PHP\n";
+
+//$nombre = "Akira";
+//$apellido = "Kuro Sawa";
+//$edad = 48;
+//echo "Hola, me llamo " . $nombre . " " . $apellido . "\n";
+
+// Para concatener es mas facil de esta manera
+//echo "Hola, me llamo $nombre $apellido y tengo $edad annios de edad\n";
+
+//echo "El resultado de 5 x 5 es: " . (5 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 18,
+    "Juana" => 25,
+    "Trinx" => 48
+];
+
+var_dump( $personas);
+print_r( $personas);
+echo "\n";
---
+++ Curso de PHP/variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+define("NUMERO_PI", 3.14);
+// define("NUMERO_PI", 14);
+
+const GRAVEDAD = 9.8;
+
+echo NUMERO_PI;
+
+echo "\n";
+
+echo GRAVEDAD;
+
+echo "\n";
